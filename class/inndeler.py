b4ce1b3517f09f7f15ecd889cb863f1ccdfc6861
Myrstad <mortenmyrstad@gmail.com>
2023-08-29 08:15:06 +0200

    inndeler start

--- class/inndeler.py
+++ class/inndeler.py
@@ -1,6 +1,13 @@
 class Inndeler:
   def __init__(self, antall_grupper, gruppestørrelse) -> None:
     pass
+  
+  @staticmethod
+  def hent_elever(fil_navn):
+    with open(fil_navn, 'r', encoding='utf-8') as fil:
+      tekst = fil.read()
+      elever = tekst.split('\n')
+      return elever
 
   def del_per_antall_grupper() -> list[str]:
     return ['hei']
@@ -9,7 +16,7 @@ class Inndeler:
     return ['hei']
   
 if __name__ == '__main__':
-  i = Inndeler()
+  i = Inndeler(None, None)
+  print(i.hent_elever('elever.txt'))
   i.del_per_antall_grupper()
-  j = Inndeler()
-  j.del_per_gruppestørrelse()
+
